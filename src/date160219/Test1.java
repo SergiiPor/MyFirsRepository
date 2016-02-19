dea84bec5a78c1a7763cdd3c232c0ed4771c1d87
SergiiPor <Sergey.al@ukr.net>
2016-02-19 18:46:26 +0200

    second commit (for test of Git's work too)

---
+++ src/date160219/Test1.java
@@ -0,0 +1,14 @@
+package date160219;
+
+/**
+ * Created by user on 19.02.2016.
+ */
+public class Test1 {
+    public int a;
+    public int b;
+
+    public Test1(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
